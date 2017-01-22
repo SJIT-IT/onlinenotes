72607e197b7268906fd06e0dc3f93f83e8f2a44d
karthikeyan-kr <varthay@gmail.com>
2017-01-22 15:41:01 +0530

    relocated javascript code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-/* 
-
-I built this login form to block the front end of most of my freelance wordpress projects during the development stage. 
-
-This is just the HTML / CSS of it but it uses wordpress's login system. 
-
-Nice and Simple
-
-*/
+function check() {
+  var a = document.getElementById("uname").value;
+  var b = document.getElementById("pwd").value;
+  c = "14itsjit";
+  d = "semester6";
+  if(a == c && b == d)
+  {
+    window.location="site/index.html";
+  }
+  else {
+    window.location="indexx.html";
+  }
+}
