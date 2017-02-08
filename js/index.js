636665b24e13165be4d91a0baa9907909a230555
lionel1704 <lionel1704@gmail.com>
2017-02-08 14:16:32 +0530

    Update Feb8

--- js/index.js
+++ js/index.js
@@ -3,9 +3,15 @@ function check() {
   var b = document.getElementById("pwd").value;
   c = "14itsjit";
   d = "semester6";
+  e = "15itsjit";
+  f = "semester4";
   if(a == c && b == d)
   {
-    window.location="site/index.html";
+    window.location = "site/index.html";
+  }
+  else if(a == e && b == f)
+  {
+    window.location = "syedjafer.github.io";
   }
   else {
     window.location="indexx.html";
